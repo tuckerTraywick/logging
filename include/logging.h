ec8713d570189b91507091d34065c253cef45594
Tucker Traywick <tucker.traywick@gmail.com>
2025-02-27 06:17:44 -0500

    Changed output format.

--- include/logging.h
+++ include/logging.h
@@ -1,47 +1,46 @@
 #ifndef LOGGING_H
 #define LOGGING_H
 
-#include <stdbool.h>
 #include <stdio.h>
 
-#define LOG_FORMAT " %s:%d:%s: "
-#define DEBUG_PREFIX   "[DEBUG]  "
-#define INFO_PREFIX    "[INFO]   "
-#define WARNING_PREFIX "[WARNING]"
-#define ERROR_PREFIX   "[ERROR]  "
+#define LOG_FORMAT " [%s:%d:%s] "
+#define DEBUG_PREFIX   "DEBUG  "
+#define INFO_PREFIX    "INFO   "
+#define WARNING_PREFIX "WARNING"
+#define ERROR_PREFIX   "ERROR  "
+#define TEST_PREFIX    "TEST  "
 
-#define logDebug(string) if (enableDebug) {fprintf(debugOut, DEBUG_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__);}
-#define logfDebug(string, ...) if (enableDebug) {fprintf(debugOut, DEBUG_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__, __VA_ARGS__);}
+#define logDebug(string) if (logDebugOutput) {fprintf(logDebugOutput, DEBUG_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__);} else {}
+#define logfDebug(string, ...) if (logDebugOutput) {fprintf(logDebugOutput, DEBUG_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__, __VA_ARGS__);} else {}
 
-#define logInfo(string) if (enableInfo) {fprintf(infoOut, INFO_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__);}
-#define logfInfo(string, ...) if (enableInfo) {fprintf(infoOut, INFO_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__, __VA_ARGS__);}
+#define logInfo(string) if (logInfoOutput) {fprintf(logInfoOutput, INFO_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__);} else {}
+#define logfInfo(string, ...) if (logInfoOutput) {fprintf(logInfoOutput, INFO_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__, __VA_ARGS__);} else {}
 
-#define logWarning(string) if (enableWarning) {fprintf(warningOut, WARNING_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__);}
-#define logfWarning(string, ...) if (enableWarning) {fprintf(warningOut, WARNING_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__, __VA_ARGS__);}
+#define logWarning(string) if (logWarningOutput) {fprintf(logWarningOutput, WARNING_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__);} else {}
+#define logfWarning(string, ...) if (logWarningOutput) {fprintf(logWarningOutput, WARNING_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__, __VA_ARGS__);} else {}
 
-#define logError(string) if (enableError) {fprintf(errorOut, ERROR_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__);}
-#define logfError(string, ...) if (enableError) {fprintf(errorOut, ERROR_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__, __VA_ARGS__);}
+#define logError(string) if (logErrorOutput) {fprintf(logErrorOutput, ERROR_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__);} else {}
+#define logfError(string, ...) if (logErrorOutput) {fprintf(logErrorOutput, ERROR_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__, __VA_ARGS__);} else {}
 
-#define putsDebug(string) if (enableDebug) {fputs((string), debugOut);}
-#define printfDebug(string, ...) if (enableDebug) {fprintf(debugOut, (string), __VA_ARGS__);}
+#define logTest(string) if (logErrorOutput) {fprintf(logErrorOutput, ERROR_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__);} else {}
+#define logfTest(string, ...) if (logErrorOutput) {fprintf(logErrorOutput, ERROR_PREFIX LOG_FORMAT string "\n", __FILE__, __LINE__, __func__, __VA_ARGS__);} else {}
 
-#define putsInfo(string) if (enableInfo) {fputs((string), infoOut);}
-#define printfInfo(string, ...) if (enableInfo) {fprintf(infoOut, (string), __VA_ARGS__);}
+#define putsDebug(string) if (logDebugOutput) {fputs((string), logDebugOutput);} else {}
+#define printfDebug(string, ...) if (logDebugOutput) {fprintf(logDebugOutput, (string), __VA_ARGS__);} else {}
 
-#define putsWarning(string) if (enableWarning) {fputs((string), debugOut);}
-#define printfWarning(string, ...) if (enableWarning) {fprintf(debugOut, (string), __VA_ARGS__);}
+#define putsInfo(string) if (logInfoOutput) {fputs((string), logInfoOutput);} else {}
+#define printfInfo(string, ...) if (logInfoOutput) {fprintf(logInfoOutput, (string), __VA_ARGS__);} else {}
 
-#define putsError(string) if (enableError) {fputs((string), debugOut);}
-#define printfError(string, ...) if (enableError) {fprintf(debugOut, (string), __VA_ARGS__);}
+#define putsWarning(string) if (logWarningOutput) {fputs((string), logDebugOutput);} else {}
+#define printfWarning(string, ...) if (logWarningOutput) {fprintf(logDebugOutput, (string), __VA_ARGS__);} else {}
+
+#define putsError(string) if (logErrorOutput) {fputs((string), logDebugOutput);} else {}
+#define printfError(string, ...) if (logErrorOutput) {fprintf(logDebugOutput, (string), __VA_ARGS__);} else {}
 
 // These variables need to be set by code that uses this header.
-FILE* debugOut = NULL;
-FILE *infoOut = NULL;
-FILE *warningOut = NULL;
-FILE *errorOut = NULL;
-bool enableDebug = true;
-bool enableInfo = true;
-bool enableWarning = true;
-bool enableError = true;
+extern FILE *logDebugOutput;
+extern FILE *logInfoOutput;
+extern FILE *logWarningOutput;
+extern FILE *logErrorOutput;
 
 #endif // LOGGING_H
--- source/example.c
+++ source/example.c
@@ -1,15 +1,15 @@
 #include "logging.h"
 
 int main(void) {
-	debugOut = stdout;
-	infoOut = stdout;
-	warningOut = stdout;
-	errorOut = stderr;
+	logDebugOutput = stdout;
+	logInfoOutput = stdout;
+	logWarningOutput = stdout;
+	logErrorOutput = stderr;
 
 	logInfo("some information");
 	logfDebug("some debug info: %d, %d", 1, 2);
 	logWarning("a warning");
-	putsInfo("some unformated info\n");
+	// putsInfo("some unformated info\n");
 	logError("an error");
 	return 0;
 }
